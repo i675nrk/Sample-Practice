eb75d82d436d44a98eed515494632ea8cac35592
Cyrah Sophia Angella Cuenca <119916620+i675nrk@users.noreply.github.com>
2023-04-13 15:27:40 +0800

    Create App.java

---
+++ Hybrid Inheritance/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        
+        System.out.println(Animal.bAnimal);
+        System.out.println("Ï'm a whale. I have " + Whale.eyes + " eyes.");
+        System.out.println("Ï'm a shark. I have " + Shark.teeth + " teeth.");
+        System.out.println("Ï'm a hybrid, a whaleshark to be exact. I also have " + Shark.teeth + " teeth. and " + Whale.eyes + " eyes.");
+    }
+}
